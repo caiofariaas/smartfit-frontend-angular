af8d9e0242456b5f2f0eec11842d6b342d344ba7
caiofariaas <caiofarias077@gmail.com>
2024-03-19 11:22:57 -0300

    dev

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,11 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule,  } from '@angular/forms';
+import { GetUnitsService } from '../../services/get-units.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [ReactiveFormsModule,],
+  imports: [ReactiveFormsModule, HttpClientModule, ],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
@@ -15,10 +17,10 @@ export class FormsComponent {
   results = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder){
-  }
+  constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService){ }
 
     ngOnInit(): void{
+        this.unitService.getAllUnits().subscribe(data => console.log(data));
         this.formGroup = this.formBuilder.group({
           hour: '',
           showClosed: false,
@@ -36,5 +38,4 @@ export class FormsComponent {
     onClean(): void{
       this.formGroup.reset()
     }
-
 }
