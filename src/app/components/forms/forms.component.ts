acfdf320507664159c0b6ab3de2fcd6bb2b629af
caiofariaas <caiofarias077@gmail.com>
2024-03-19 10:45:29 -0300

    feat: add form control

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule,  } from '@angular/forms';
+
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
+
 export class FormsComponent {
 
   results = [];
+  formGroup!: FormGroup;
 
-  
+  constructor(private formBuilder: FormBuilder){
+  }
+
+    ngOnInit(): void{
+        this.formGroup = this.formBuilder.group({
+          hour: '',
+          showClosed: false,
+        })
+    }
+
+    /* Função para pegar os valores do formulário */
+
+    onSubmit(): void{
+      console.log(this.formGroup.value)
+    }
+
+    /* Função que limpa os filtros */
+
+    onClean(): void{
+      this.formGroup.reset()
+    }
 
 }
